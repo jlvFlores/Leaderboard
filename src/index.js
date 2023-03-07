96b0c2d02bac11cea1c4190328b0cb4efdec2afd
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-03-07 17:01:20 -0600

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -10,42 +10,42 @@ const removeChildren = (parent) => {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
-}
+};
 
 const getScores = async () => {
   await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Kam0cZKamTISyp4S5fjz/scores/', {
     headers: {
-       'Accept': 'application/json'
-    }
+      Accept: 'application/json',
+    },
   })
-    .then(response => response.text())
-    .then(response => {
+    .then((response) => response.text())
+    .then((response) => {
       removeChildren(recentScores);
       const results = JSON.parse(response);
-      results['result'].forEach(result => {
+      results.result.forEach((result) => {
         recentScores.insertAdjacentHTML('beforeend', `
-          <p>${result['user']}: ${result['score']}</p>
+          <p>${result.user}: ${result.score}</p>
         `);
       });
     });
-}
+};
 
 const postScore = async () => {
   await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Kam0cZKamTISyp4S5fjz/scores/', {
     method: 'POST',
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ 
-      "user": name.value,
-      "score": +score.value
-    })
+    body: JSON.stringify({
+      user: name.value,
+      score: +score.value,
+    }),
   })
-   .then(response => response.json())
+    .then((response) => response.json());
   name.value = '';
   score.value = '';
-}
+};
 
 refresh.addEventListener('click', () => getScores());
 add.addEventListener('click', () => postScore());
